a03e0a41c1c83585e1b64bf032b6aa5c7a8d79c1
Kred_cl <derek.folch@gmail.com>
2024-03-13 16:09:21 -0300

    version 1.1

--- AppCoder/forms.py
+++ AppCoder/forms.py
@@ -13,11 +13,11 @@ class DueñoForm(forms.ModelForm):
         model = Dueño
         fields = ['nombre', 'apellido', 'email', 'restorant']
     
+class restoForm(forms.ModelForm):
+    class Meta:
+        model = resto
+        fields = ['nombre', 'direccion', 'email']
 
-class restoForm(forms.Form):
-    nombre = forms.CharField(max_length=100)
-    direccion = forms.CharField(max_length=100)
-    email = forms.EmailField()
 
 
 
